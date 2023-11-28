96ffe9e8528590a3c77a45aee3081ddd88b9acc0
Levivus <levi.varsanyi@gmail.com>
2023-11-28 12:24:12 -0800

    definition adjustments

--- src/robot_controller.py
+++ src/robot_controller.py
@@ -22,6 +22,16 @@ IMAGE_HEIGHT = 720
 IMAGE_WIDTH = 1280
 IMAGE_DEPTH = 3
 
+#CONSTANTS
+CROP_AMOUNT = 250
+LOSS_FACTOR = 1
+LOWER_PINK = np.array([200, 0, 200], dtype=np.uint8)
+UPPER_PINK = np.array([255, 150, 255], dtype=np.uint8)
+LOWER_RED = np.array([0, 0, 200], dtype=np.uint8)
+UPPER_RED = np.array([100, 100, 255], dtype=np.uint8)
+LOWER_COLOR = np.array([0, 0, 78], dtype=np.uint8)
+UPPER_COLOR = np.array([125, 135, 255], dtype=np.uint8)
+
 
 # Some random states, will update later as needed for example, may have
 # multiple driving states (depending on location), a pedestrian stop state,
@@ -58,16 +68,17 @@ class topic_publisher:
         print("after wait", time.time())
         self.score_pub.publish("%s,%s,0,NA" % (TEAM_NAME, PASSWORD))
         self.running = True
-        self.state = State.DRIVING
         print("init done", time.time())
+        self.spawn_position(HOME)
 
     def clock_callback(self, data):
         """Callback for the clock subscriber
         Publishes the score to the score tracker node when the competition ends
         """
-        if self.image_difference < 7000:
-            self.spawn_position(HOME)
-            print("Respawned")
+        # if self.image_difference < 7000:
+        #     self.spawn_position(HOME)
+        #     print("Respawned")
+        print("Clock")
         if self.running and data.clock.secs - self.time_start > END_TIME:
             self.running = False
             self.score_pub.publish("%s,%s,-1,NA" % (TEAM_NAME, PASSWORD))
@@ -99,12 +110,10 @@ class topic_publisher:
         """
         # Depending on needs of states, the state datatype may have to be changed to allow for multiple states at once
 
-        if (
-            not self.running
-        ):  # if the competition is over, stop the robot, do nothing else
+        if (not self.running):  # if the competition is over, stop the robot, do nothing else
             return State.SHUTDOWN, data
-
-        return self.state, data
+        else:
+            return State.DRIVING, data
 
     def driving(self, data):
         """Function for the DRIVING state"""
@@ -115,41 +124,34 @@ class topic_publisher:
             print(e)
 
         height, width = cv_image.shape[:2]
-
-        crop_amount = 250
-        loss_factor = 1
+        
         contour_colour = (0, 255, 0)
         lost = False
         lost_left = False
         lost_right = False
 
-        lower_pink = np.array([200, 0, 200], dtype=np.uint8)
-        upper_pink = np.array([255, 150, 255], dtype=np.uint8)
-        pink_mask = cv2.inRange(cv_image, lower_pink, upper_pink)
+        pink_mask = cv2.inRange(cv_image, LOWER_PINK, UPPER_PINK)
         pink_pixel_count = cv2.countNonZero(pink_mask)
         print("pink pixel count:", pink_pixel_count)
 
-        lower_red = np.array([0, 0, 200], dtype=np.uint8)
-        upper_red = np.array([100, 100, 255], dtype=np.uint8)
-        red_mask = cv2.inRange(cv_image, lower_red, upper_red)
+        red_mask = cv2.inRange(cv_image, LOWER_RED, UPPER_RED)
         red_pixel_count = cv2.countNonZero(red_mask)
         print("red pixel count:", red_pixel_count)
-        if red_pixel_count > 500:
-            self.state = State.PEDESTRIAN
-            return
+        # if red_pixel_count > 500:
+        #     TODO: Move this type of check into get_state()
+        #     self.state = State.PEDESTRIAN
+        #     return
 
-        lower_color = np.array([0, 0, 78], dtype=np.uint8)
-        upper_color = np.array([125, 135, 255], dtype=np.uint8)
-        masktest = cv2.inRange(cv_image, lower_color, upper_color)
+        masktest = cv2.inRange(cv_image, LOWER_COLOR, UPPER_COLOR)
 
         # image processing, including cropping and thresholding
         gray_image = cv2.cvtColor(cv_image, cv2.COLOR_BGR2GRAY)
         _, black_image = cv2.threshold(gray_image, 100, 255, type=cv2.THRESH_BINARY)
         black_image = cv2.bitwise_not(black_image)
-        cropped_image = black_image[height - crop_amount :, :]
+        cropped_image = black_image[height - CROP_AMOUNT :, :]
         _, mask = cv2.threshold(cropped_image, 128, 255, cv2.THRESH_BINARY)
 
-        mask = masktest[height - crop_amount :, :]
+        mask = masktest[height - CROP_AMOUNT :, :]
 
         # find where the road x position is
         contours, _ = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
@@ -163,28 +165,28 @@ class topic_publisher:
 
             cv2.circle(
                 cv_image,
-                (bottom_left[0][0], bottom_left[0][1] + (height - crop_amount)),
+                (bottom_left[0][0], bottom_left[0][1] + (height - CROP_AMOUNT)),
                 7,
                 (0, 0, 255),
                 -1,
             )
             cv2.circle(
                 cv_image,
-                (top_left[0][0], top_left[0][1] + (height - crop_amount)),
+                (top_left[0][0], top_left[0][1] + (height - CROP_AMOUNT)),
                 7,
                 (0, 0, 0),
                 -1,
             )
             cv2.circle(
                 cv_image,
-                (bottom_right[0][0], bottom_right[0][1] + (height - crop_amount)),
+                (bottom_right[0][0], bottom_right[0][1] + (height - CROP_AMOUNT)),
                 7,
                 (255, 0, 0),
                 -1,
             )
             cv2.circle(
                 cv_image,
-                (top_right[0][0], top_right[0][1] + (height - crop_amount)),
+                (top_right[0][0], top_right[0][1] + (height - CROP_AMOUNT)),
                 7,
                 (0, 255, 0),
                 -1,
@@ -213,10 +215,10 @@ class topic_publisher:
             centroid_x = int(M["m10"] / M["m00"])
             # draw circle onto image
             cv2.circle(
-                cv_image, (centroid_x, height - crop_amount // 2), 5, (0, 0, 255), -1
+                cv_image, (centroid_x, height - CROP_AMOUNT // 2), 5, (0, 0, 255), -1
             )
             # shift contour points to account for cropping
-            y_offset = height - crop_amount
+            y_offset = height - CROP_AMOUNT
             largest_contour[:, 0, 1] += y_offset
             # draw contour onto image
             cv2.drawContours(cv_image, [largest_contour], -1, contour_colour, 2)
@@ -224,14 +226,14 @@ class topic_publisher:
             error = centroid_x - width / 2  # positive means the car should turn left
 
             if lost_left:
-                error = abs(self.previous_error) + loss_factor
+                error = abs(self.previous_error) + LOSS_FACTOR
             if lost_right:
-                error = -abs(self.previous_error) - loss_factor
+                error = -abs(self.previous_error) - LOSS_FACTOR
             if lost:
                 error = (
-                    self.previous_error - loss_factor
+                    self.previous_error - LOSS_FACTOR
                     if self.previous_error < 0
-                    else self.previous_error + loss_factor
+                    else self.previous_error + LOSS_FACTOR
                 )
         else:  # no contours detected, so set error directly
             error = -width / 2 if self.previous_error < 0 else width / 2
