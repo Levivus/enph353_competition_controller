7a380f482978c650dd6d94da0ee1466c133e7701
Levivus <levi.varsanyi@gmail.com>
2023-11-28 15:18:52 -0800

    adjust lost road logic and fix delayed start

--- src/robot_controller.py
+++ src/robot_controller.py
@@ -17,20 +17,26 @@ import numpy as np
 TEAM_NAME = "MchnEarn"
 PASSWORD = "pswd"
 END_TIME = 5000000
-HOME = [5.5, 2.0, 0.1, 0.0, 0.0, np.sqrt(2), -np.sqrt(2)]
+HOME = [5.5, 2.6, 0.1, 0.0, 0.0, np.sqrt(2), -np.sqrt(2)]
 IMAGE_HEIGHT = 720
 IMAGE_WIDTH = 1280
 IMAGE_DEPTH = 3
 
 #CONSTANTS
 CROP_AMOUNT = 250
-LOSS_FACTOR = 1
+LOSS_FACTOR = 250
 LOWER_PINK = np.array([200, 0, 200], dtype=np.uint8)
 UPPER_PINK = np.array([255, 150, 255], dtype=np.uint8)
 LOWER_RED = np.array([0, 0, 200], dtype=np.uint8)
 UPPER_RED = np.array([100, 100, 255], dtype=np.uint8)
 LOWER_COLOR = np.array([0, 0, 78], dtype=np.uint8)
 UPPER_COLOR = np.array([125, 135, 255], dtype=np.uint8)
+
+#PID
+Kp = 0.017
+Kd = 0.003
+MAX_SPEED = 0.2
+SPEED_DROP = 0.00055
 
 
 # Some random states, will update later as needed for example, may have
@@ -46,6 +52,7 @@ class State(Enum):
 
 class topic_publisher:
     def __init__(self):
+        self.running = False  # Prevent callback from running before competition starts
         self.bridge = CvBridge()
         self.previous_error = -100
         self.image_sub = rospy.Subscriber(
@@ -53,22 +60,15 @@ class topic_publisher:
         )
         self.move_pub = rospy.Publisher("R1/cmd_vel", Twist, queue_size=1)
         self.score_pub = rospy.Publisher("/score_tracker", String, queue_size=1)
-        self.clock_sub = rospy.Subscriber(
-            "/clock", Clock, self.clock_callback, queue_size=1
-        )
-        self.running = False  # Prevent callback from running before competition starts
+        self.clock_sub = rospy.Subscriber("/clock", Clock, self.clock_callback)
         self.image_difference = 100000
         self.last_image = np.zeros(
             (IMAGE_HEIGHT, IMAGE_WIDTH, IMAGE_DEPTH), dtype=np.uint8
         )
-        print("before sleep", time.time())
         time.sleep(1)
-        print("after sleep, before wait", time.time())
         self.time_start = rospy.wait_for_message("/clock", Clock).clock.secs
-        print("after wait", time.time())
         self.score_pub.publish("%s,%s,0,NA" % (TEAM_NAME, PASSWORD))
         self.running = True
-        print("init done", time.time())
         self.spawn_position(HOME)
 
     def clock_callback(self, data):
@@ -78,7 +78,6 @@ class topic_publisher:
         # if self.image_difference < 7000:
         #     self.spawn_position(HOME)
         #     print("Respawned")
-        print("Clock")
         if self.running and data.clock.secs - self.time_start > END_TIME:
             self.running = False
             self.score_pub.publish("%s,%s,-1,NA" % (TEAM_NAME, PASSWORD))
@@ -90,17 +89,17 @@ class topic_publisher:
         """
         state, new_data = self.get_state(data)
 
-        new_image = np.array(self.bridge.imgmsg_to_cv2(new_data, "bgr8"))
-        self.image_difference = cv2.norm(self.last_image, new_image, cv2.NORM_L2)
-
-        if (
-            state == State.SHUTDOWN
-        ):  # TODO: shut down the script? - may not even need this state once this is implemented
+        if (state == State.SHUTDOWN):  # TODO: shut down the script? - may not even need this state once this is implemented
             self.move_pub.publish(Twist())
-        elif state == State.DRIVING:
+            return
+
+        new_image = np.array(self.bridge.imgmsg_to_cv2(new_data, "bgr8"))
+        # self.image_difference = cv2.norm(self.last_image, new_image, cv2.NORM_L2)
+
+        if state == State.DRIVING:
             self.driving(new_data)
-        elif state == State.PEDESTRIAN:
-            self.pedestrian(new_data)
+        # elif state == State.PEDESTRIAN:
+        #     self.pedestrian(new_data)
 
         self.last_image = np.array(self.bridge.imgmsg_to_cv2(new_data, "bgr8"))
 
@@ -126,9 +125,7 @@ class topic_publisher:
         height, width = cv_image.shape[:2]
         
         contour_colour = (0, 255, 0)
-        lost = False
         lost_left = False
-        lost_right = False
 
         pink_mask = cv2.inRange(cv_image, LOWER_PINK, UPPER_PINK)
         pink_pixel_count = cv2.countNonZero(pink_mask)
@@ -138,20 +135,22 @@ class topic_publisher:
         red_pixel_count = cv2.countNonZero(red_mask)
         print("red pixel count:", red_pixel_count)
         # if red_pixel_count > 500:
-        #     TODO: Move this type of check into get_state()
+        #     TODO: Move this type of check into get_state()?
         #     self.state = State.PEDESTRIAN
         #     return
 
-        masktest = cv2.inRange(cv_image, LOWER_COLOR, UPPER_COLOR)
+        mask = cv2.inRange(cv_image, LOWER_COLOR, UPPER_COLOR)
 
         # image processing, including cropping and thresholding
-        gray_image = cv2.cvtColor(cv_image, cv2.COLOR_BGR2GRAY)
-        _, black_image = cv2.threshold(gray_image, 100, 255, type=cv2.THRESH_BINARY)
-        black_image = cv2.bitwise_not(black_image)
-        cropped_image = black_image[height - CROP_AMOUNT :, :]
-        _, mask = cv2.threshold(cropped_image, 128, 255, cv2.THRESH_BINARY)
+        # gray_image = cv2.cvtColor(cv_image, cv2.COLOR_BGR2GRAY)
+        # _, black_image = cv2.threshold(gray_image, 100, 255, type=cv2.THRESH_BINARY)
+        # black_image = cv2.bitwise_not(black_image)
+        # cropped_image = black_image[height - CROP_AMOUNT :, :]
+        # _, mask = cv2.threshold(cropped_image, 128, 255, cv2.THRESH_BINARY)
 
-        mask = masktest[height - CROP_AMOUNT :, :]
+        mask = mask[height - CROP_AMOUNT :, :]
+
+        
 
         # find where the road x position is
         contours, _ = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
@@ -159,103 +158,126 @@ class topic_publisher:
         error = 0
         if len(contours) > 0:
             largest_contour = max(contours, key=cv2.contourArea)
-            bottom_left, top_left, bottom_right, top_right = self.find_corner_points(
-                largest_contour
-            )
-
-            cv2.circle(
-                cv_image,
-                (bottom_left[0][0], bottom_left[0][1] + (height - CROP_AMOUNT)),
-                7,
-                (0, 0, 255),
-                -1,
-            )
-            cv2.circle(
-                cv_image,
-                (top_left[0][0], top_left[0][1] + (height - CROP_AMOUNT)),
-                7,
-                (0, 0, 0),
-                -1,
-            )
-            cv2.circle(
-                cv_image,
-                (bottom_right[0][0], bottom_right[0][1] + (height - CROP_AMOUNT)),
-                7,
-                (255, 0, 0),
-                -1,
-            )
-            cv2.circle(
-                cv_image,
-                (top_right[0][0], top_right[0][1] + (height - CROP_AMOUNT)),
-                7,
-                (0, 255, 0),
-                -1,
-            )
-
-            for point in largest_contour:
-                cv2.circle(
-                    cv_image, (point[0][0], point[0][1]), 10, (255, 255, 255), -1
-                )
-
-            if bottom_left[0][0] == top_left[0][0]:
+            # check if any point is at [0][0]
+            print(largest_contour)
+            print(largest_contour[0][0])
+            
+            if largest_contour[0][0][0] == 0 and largest_contour[0][0][1] == 0:
                 lost_left = True
-                contour_colour = (0, 255, 255)
-            if bottom_right[0][0] == top_right[0][0]:
-                lost_right = True
-                contour_colour = (0, 165, 255)
-            if lost_left and lost_right:
-                lost = True
-                contour_colour = (0, 0, 255)
-            print("lost:", lost, "lost left:", lost_left, "lost right:", lost_right)
+                # print("lost left")
+                # contour_colour = (0, 255, 255)
 
-            M = cv2.moments(largest_contour)
-            if M["m00"] == 0:
-                # print(contours)
-                return
-            centroid_x = int(M["m10"] / M["m00"])
-            # draw circle onto image
-            cv2.circle(
-                cv_image, (centroid_x, height - CROP_AMOUNT // 2), 5, (0, 0, 255), -1
-            )
-            # shift contour points to account for cropping
-            y_offset = height - CROP_AMOUNT
-            largest_contour[:, 0, 1] += y_offset
-            # draw contour onto image
-            cv2.drawContours(cv_image, [largest_contour], -1, contour_colour, 2)
-            # calculate the error
-            error = centroid_x - width / 2  # positive means the car should turn left
+            # bottom_left, top_left, bottom_right, top_right = self.find_corner_points(
+            #     largest_contour
+            # )
 
-            if lost_left:
-                error = abs(self.previous_error) + LOSS_FACTOR
-            if lost_right:
-                error = -abs(self.previous_error) - LOSS_FACTOR
-            if lost:
-                error = (
-                    self.previous_error - LOSS_FACTOR
-                    if self.previous_error < 0
-                    else self.previous_error + LOSS_FACTOR
-                )
+            # cv2.circle(
+            #     cv_image,
+            #     (bottom_left[0][0], bottom_left[0][1] + (height - CROP_AMOUNT)),
+            #     7,
+            #     (0, 0, 255),
+            #     -1,
+            # )
+            # cv2.circle(
+            #     cv_image,
+            #     (top_left[0][0], top_left[0][1] + (height - CROP_AMOUNT)),
+            #     7,
+            #     (0, 0, 0),
+            #     -1,
+            # )
+            # cv2.circle(
+            #     cv_image,
+            #     (bottom_right[0][0], bottom_right[0][1] + (height - CROP_AMOUNT)),
+            #     7,
+            #     (255, 0, 0),
+            #     -1,
+            # )
+            # cv2.circle(
+            #     cv_image,
+            #     (top_right[0][0], top_right[0][1] + (height - CROP_AMOUNT)),
+            #     7,
+            #     (0, 255, 0),
+            #     -1,
+            # )
+
+            # for point in largest_contour:
+            #     cv2.circle(
+            #         cv_image, (point[0][0], point[0][1]), 10, (255, 255, 255), -1
+            #     )
+
+            # if bottom_left[0][0] == top_left[0][0]:
+            #     lost_left = True
+            #     contour_colour = (0, 255, 255)
+            # if bottom_right[0][0] == top_right[0][0]:
+            #     lost_right = True
+            #     contour_colour = (0, 165, 255)
+            # if lost_left and lost_right:
+            #     lost = True
+            #     contour_colour = (0, 0, 255)
+            # print("lost:", lost, "lost left:", lost_left, "lost right:", lost_right)
+            else:
+                M = cv2.moments(largest_contour)
+                if M["m00"] == 0:
+                    # print(contours)
+                    return
+                centroid_x = int(M["m10"] / M["m00"])
+                # draw circle onto image
+                # cv2.circle(
+                #     cv_image, (centroid_x, height - CROP_AMOUNT // 2), 5, (0, 0, 255), -1
+                # )
+                # shift contour points to account for cropping
+                y_offset = height - CROP_AMOUNT
+                largest_contour[:, 0, 1] += y_offset
+                # draw contour onto image
+                cv2.drawContours(cv_image, [largest_contour], -1, contour_colour, 2)
+                # calculate the error
+                error = centroid_x - width / 2  # positive means the car should turn right
+
+
+                # if lost_left:
+                #     error = abs(self.previous_error) + LOSS_FACTOR
+                # if lost_right:
+                #     error = -abs(self.previous_error) - LOSS_FACTOR
+                # if lost:
+                #     error = (
+                #         self.previous_error - LOSS_FACTOR
+                #         if self.previous_error < 0
+                #         else self.previous_error + LOSS_FACTOR
+                #     )
         else:  # no contours detected, so set error directly
             error = -width / 2 if self.previous_error < 0 else width / 2
 
+        if lost_left: # This will bias the car to the left if both sides are lost
+            error = -LOSS_FACTOR
+            # write onto image
+            cv2.putText(
+                cv_image,
+                "Lost LEFT",
+                (10, 50),
+                cv2.FONT_HERSHEY_SIMPLEX,
+                1,
+                (0, 0, 255),
+                2,
+                cv2.LINE_AA,
+            )
+            
+
         cv2.imshow("Driving Image", cv_image)
-        cv2.imshow("Road Mask Test", mask)
+        # cv2.imshow("Road Mask Test", mask)
         cv2.waitKey(3)
 
         # PID controller
         move = Twist()
 
         print("Error", error)
-        Kp = 0.017
-        Kd = 0.003
         derivative = error - self.previous_error
         self.previous_error = error
 
         move.angular.z = -(Kp * error + Kd * derivative)
         print("angular speed:", move.angular.z, "\n")
+
         # decrease linear speed as angular speed increases from a max of 3 down to 1.xx? if abs(error) > 400
-        move.linear.x = max(0, 0.1 - 0.00065 * abs(error))
-        # move.linear.x = 0.2
+        move.linear.x = max(0, MAX_SPEED - SPEED_DROP * abs(error))
 
         self.move_pub.publish(move)
 
